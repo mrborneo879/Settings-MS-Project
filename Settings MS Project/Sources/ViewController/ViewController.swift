7c65439171d05ecf02e8f6ff79b9a80423d187f1
Alexander Stepanov <alexanderlawyer97@gmail.com>
2023-11-25 19:55:15 +0100

    Files, folders cleaning & configuration

---
+++ Settings MS Project/Sources/ViewController/ViewController.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray
+    }
+}
+
