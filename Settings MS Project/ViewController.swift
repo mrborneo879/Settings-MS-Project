ab6290659d306401011d814a3b8278705369da09
Alexander Stepanov <alexanderlawyer97@gmail.com>
2023-11-25 19:41:36 +0100

    Initial Commit

---
+++ Settings MS Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Settings MS Project
+//
+//  Created by Mac Alexander on 25.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
