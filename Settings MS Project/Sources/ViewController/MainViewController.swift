4cbc4bb89f2fb18ecfee235dea2390b1b7f0a838
Alexander Stepanov <alexanderlawyer97@gmail.com>
2023-12-03 19:17:24 +0100

    Prepare ViewController.

--- Settings MS Project/Sources/ViewController/MainViewController.swift
+++ Settings MS Project/Sources/ViewController/MainViewController.swift
@@ -1,1 +1,49 @@
 
+import UIKit
+import SnapKit
+
+final class MainViewController: UIViewController {
+    
+    // MARK: - State
+    
+    //var settings: [[Setting]]?
+    var identifier: String = "Cell"
+    
+    // MARK: - UIElements
+    
+    private lazy var tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        //table.dataSource = self
+        //table.delegate = self
+        //table.register(MainTableViewCell.self, forCellReuseIdentifier: identifier)
+        return table
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Настройки"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        setupHierarchy()
+        setupLayout()
+        //useModelData()
+    }
+    
+    // MARK: - Setup & Layout
+    
+    //private func useModelData() {
+    //    settings = Setting.allSettings
+    //}
+    
+    private func setupHierarchy() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupLayout() {
+        tableView.snp.makeConstraints { make in
+            make.top.left.bottom.right.equalTo(view)
+        }
+    }
+}
